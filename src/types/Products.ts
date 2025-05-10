be5a6f354cf3dc08bd4e215bcff65d498b1c5186
Fredydev05 <cespedesmatias123@gmail.com>
2025-05-09 22:02:26 -0300

    add type of products for the carousel

---
+++ src/types/Products.ts
@@ -0,0 +1,97 @@
+export type Product = {
+  image: string;
+  title: string;
+  link: string;
+};
+
+export type Products = Product[];
+
+
+export const products: Products = [
+  {
+    image: "/momentaneo.png",
+    title: "Feiyu series negro - plata",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Producto 1",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    link: "https://facebook.com",
+  },
+ 
+];
