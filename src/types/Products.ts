2a738b25b209042130990d658e290ccdc86d2061
Fredydev05 <cespedesmatias123@gmail.com>
2025-05-18 17:39:46 -0300

    add new type for products

--- src/types/Products.ts
+++ src/types/Products.ts
@@ -1,6 +1,7 @@
 export type Product = {
   image: string;
   title: string;
+  small: string;
 };
 
 export type Products = Product[];
@@ -8,72 +9,89 @@ export type Products = Product[];
 
 export const products: Products = [
   {
-    image: "/momentaneo.png",
+    image: "/products/producto1.webp",
     title: "Feiyu series negro - plata",
+    small: "Material inoxidable 304 - 75 X 40"
   },
   {
-    image: "/momentaneo.png",
-    title: "Producto 1",
+    image: "/products/producto2.webp",
+    title: "Undermount Negro",
+    small: "Material inoxidable 304 60 X 40"
   },
   {
-    image: "/momentaneo.png",
-    title: "Otro producto",
-  },
-  {
-    image: "/momentaneo.png",
-    title: "Otro producto",
-  },
-  {
-    image: "/momentaneo.png",
-    title: "Otro producto",
+    image: "/products/producto3.webp",
+    title: "Undermount Plata",
+    small: "Material inoxidable 304 60 X 40"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
   },
   {
     image: "/momentaneo.png",
     title: "Otro producto",
+    small: "Material prueba 123"
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    small: "Material prueba 123"
+  },
+  {
+    image: "/momentaneo.png",
+    title: "Otro producto",
+    small: "Material prueba 123"
   },
  
 ];
