84a46ce8936d8d923210c2ee21d4d83f0b7e0372
Fredydev05 <cespedesmatias123@gmail.com>
2025-06-06 22:39:24 -0300

    fix cover img and add noStock advise in case stock is 0

--- src/components/animations/ProductCarousel.jsx
+++ src/components/animations/ProductCarousel.jsx
@@ -10,7 +10,7 @@ const ProductCarousel = ({ products }) => {
   const pauseTimeoutRef = useRef(null);
   const scrollEndTimeoutRef = useRef(null);
 
-  const itemWidth = 366; 
+  const itemWidth = 366;
 
   // Recentrar la card más cercana al centro
   const snapToNearest = () => {
@@ -90,26 +90,33 @@ const ProductCarousel = ({ products }) => {
         {products.map((product, i) => (
           <div
             key={i}
-            className="w-[350px] flex-shrink-0 bg-[#F8F8F8] shadow p-3 h-auto pb-6 scrollSnapAlign"
+            className="w-[350px] flex-shrink-0 bg-[#F8F8F8] shadow p-3 h-auto pb-6 scrollSnapAlign overflow-hidden"
             style={{ scrollSnapAlign: "center" }}
           >
+            {!product.inStock && (
+              <div className="relative  top-0 -left-10 bg-red-600 text-white text-xs px-8 py-1 rotate-[-45deg] shadow-lg w-fit">
+                Sin stock
+              </div>
+            )}
             <img
               draggable={false}
               src={product.image}
               alt={product.title}
-              className="w-full h-[300px] object-contain"
+              className="w-full h-[300px] object-cover"
             />
-            <div className="mb-6">
+            <div className="mb-6 mt-4">
               <h3 className="text-xl font-semibold text-[#262626]">
                 {product.title}
               </h3>
-              <small className="text-[#606060] text-sm">
-                {product.small}
-              </small>
+              <small className="text-[#606060] text-sm">{product.small}</small>
             </div>
             <a
-              href={`https://wa.me/595984955125?text=Hola!%20me%20interesaria%20cotizar%20el%20${encodeURIComponent(product.title)}`} target="_blank" 
-              className="text-white bg-[#25D366] shadow-xl shadow-[#262626]/20 p-2 px-4 rounded-xl hover:brightness-75 transition w-fit flex items-center gap-2">
+              href={`https://wa.me/595984955125?text=Hola!%20me%20interesaria%20cotizar%20el%20${encodeURIComponent(
+                product.title
+              )}`}
+              target="_blank"
+              className="text-white bg-[#25D366] shadow-xl shadow-[#262626]/20 p-2 px-4 rounded-xl hover:brightness-75 transition w-fit flex items-center gap-2"
+            >
               <iconify-icon
                 icon="ic:round-whatsapp"
                 class="text-xl"
